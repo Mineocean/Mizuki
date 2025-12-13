acf98627bb1a60ac48a0b83bad0d94d6f1c0d5bc
Yozakura Misaka <MisakaOI@outlook.com>
2025-12-13 22:50:44 +0800

    重新再传

--- src/config.ts
+++ src/config.ts
@@ -285,7 +285,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "Misaka Yozakura",
 	bio: "创造奇迹，改变世界",
 	typewriter: {
